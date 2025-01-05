e5dcaacc0dd0dfe012e335df5c7866e9cb58a433
Spestly <aegisium@proton.me>
2025-01-06 10:59:16 +1100

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -117,7 +117,7 @@ const teamMembers = [
    {
     name: 'Kazex',
     role: 'Media Director',
-    image: '/team/Kazex.jpg',
+    image: '/team/Kazex.png',
     bio: 'Kazex has a passion for creating innovative AI models.',
     social: {},
     hoverEffect: 'hover:scale-105 hover:rotate-1 hover:shadow-xl hover:shadow-cyan-500/40'
